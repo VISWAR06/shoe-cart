77149714e7692fcf17c8dc78df7b13fab6bd325e
VISWAR06 <viswa.ec22@bitsathy.ac.in>
2025-05-03 17:05:26 +0530

    finished

--- src/coompoents/Cart.js
+++ src/coompoents/Cart.js
@@ -1,27 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
-export default function Cart({cart,setCart}){
-    const [total,setTotal]=useState(0)
-    return(
-    <>
-    <h1>Cart Products</h1>
+export default function Cart({ cart, setCart }) {
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    setTotal(cart.reduce((acc, curr) => acc + parseInt(curr.amount), 0));
+  }, [cart]);
+
+  return (
     <div>
-        {
-            cart.map((product)=>(
-                <div key={product.id}>
-                    <div>
-                    <img src={product.image}/>
-                    <div>
-                        <h3>{product.name}</h3>
-                        <p>{product.amount}</p>
-                    </div>
-                    </div>
-                    </div>
-               
-            )
-        )
-        }
+      <h2 className="font-bold text-xl mb-4">Products</h2>
+      <div>
+        {cart.map((product) => (
+          <div key={product.id} className="border border-violet-600 h-32 w-full mb-4 p-2 flex items-center gap-4">
+            <img src={product.image} alt={product.name} className="h-full w-auto object-cover" />
+            <div>
+              <h3 className="font-semibold">{product.name}</h3>
+              <p>Price: ${product.amount}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="mt-4 font-bold text-lg">Total: ${total}</div>
     </div>
-    </>
-    )
+  );
 }
